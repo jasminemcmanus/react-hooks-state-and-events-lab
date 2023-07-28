d37451bb15a32e062fff93073cbd95fcf9ddf1e6
Jasmine McManus <jasmine.mcmanus@outlook.com>
2023-07-28 13:13:36 +1000

    done with assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  function handleAddToCart() {
+    setInCart(!inCart);
+  }
+
+  const appClass = inCart ? "in-cart" : ""
+
   return (
-    <li className="">
+    <li className={appClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToCart}>{inCart ? "Remove from cart" : "Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,33 @@
 import React from "react";
 import Item from "./Item";
+import { useState } from "react";
 
 function ShoppingList({ items }) {
+  const [filterBy, setFilterBy] = useState("All");
+
+  console.log(items);
+
+  function filterItems(event) {
+    setFilterBy(event.target.value);
+    console.log(filterBy);
+  }
+
+  const itemsToDisplay = items.filter((item) => {
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return item.category === filterBy;
+    }
+  });
+
+  const itemList = itemsToDisplay.map((item) => (
+    <Item key={item.id} name={item.name} category={item.category} />
+  ))
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={filterItems}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +35,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {itemList}
       </ul>
     </div>
   );
